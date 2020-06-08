1ac02cb34290995f8bdbe110e57362429ad785a1
ovsyannikov-88 <ovsyannikov-88@mail.ua>
2020-06-08 18:02:22 +0300

    1

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
